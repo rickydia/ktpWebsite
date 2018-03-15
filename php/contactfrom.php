a38c2646ba68cae9bf9ea81d4d3573650304f82b
rickydia <rickydia@umich.edu>
2018-03-15 16:26:31 -0400

    tried to do shit

--- php/contactfrom.php
+++ php/contactfrom.php
@@ -43,7 +43,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "ktp-board@umich.edu";
 
 
 // Configuration option.
@@ -51,7 +51,7 @@ $address = "example@example.net";
 
 // Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
-$e_subject = 'You have been contacted by ' . $name . '.';
+$e_subject = 'KTP Website Message - ' . $name . '.';
 
 
 // Configuration option.
