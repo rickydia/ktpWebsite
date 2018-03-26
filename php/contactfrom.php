671f9c71ffa6627036bb9f5e5e29f84362985a33
rickydia <rickydia@umich.edu>
2018-03-26 16:12:59 -0400

    contact

--- php/contactfrom.php
+++ php/contactfrom.php
@@ -18,11 +18,11 @@ $comments = $_POST['comments'];
 if(trim($name) == '') {
 	echo '<div class="error_message">You must enter your name.</div>';
 	exit();
-	
+
 } else if(trim($email) == '') {
 	echo '<div class="error_message">Please enter a valid email address.</div>';
 	exit();
-	
+
 } else if(!isEmail($email)) {
 	echo '<div class="error_message">You have entered an invalid e-mail address. Please try again.</div>';
 	exit();
